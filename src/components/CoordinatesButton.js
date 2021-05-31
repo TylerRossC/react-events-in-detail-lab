20c1ca7834a3b7883b2d4919a271c10e86dff1f4
tyler <tylercharawell@gmail.com>
2021-05-31 11:02:13 -0700

    commit tree e1de321a44870ac792ad8bcb7d5018b001e95a2d

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,24 @@
-// Code CoordinatesButton Component Here
+import React, {Component} from 'react' 
+
+
+class CoordinatesButton extends Component {
+
+handleCoordinates = (e) => {
+let x = e.clientX
+let y = e.clientY
+let coords = [x, y]
+this.props.onReceiveCoordinates(coords)
+}
+
+render() {
+    return(
+        <button 
+        onClick = {this.handleCoordinates}
+        >
+            Receive 
+        </button>
+        )
+    }
+}
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,25 @@
-// Code DelayedButton Component Here
+import React, {Component} from 'react' 
+
+
+class DelayedButton extends Component {
+
+handleDelay = (e) => {
+    let e = this.props.delay
+    setTimeout(() => {
+        this.props.onDelayedClick(e)
+    })
+    
+}
+
+render() {
+    return(
+        <button
+        onClick = {this.handleDelay}
+        >
+            Delayed
+        </button>
+        )
+    }
+}
+
+export default DelayedButton
